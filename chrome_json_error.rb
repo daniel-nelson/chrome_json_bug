468fa3aa1a25d98503fd4237477ce914d4c6c492
Daniel Nelson <dnelson@centresource.com>
2011-12-29 15:28:25 -0600

    match Rails' default cache control settings

--- chrome_json_error.rb
+++ chrome_json_error.rb
@@ -7,9 +7,9 @@ Bundler.require
 set :views, File.join(File.dirname(__FILE__), 'views')
 
 get "/docs" do
+  cache_control :private, :must_revalidate, :max_age => 0
   headers('Vary' => 'Accept')
   if request.accept.include?('application/json')
-    # cache_control :private, :must_revalidate, :no_cache, :no_store, :max_age => 0
     content_type :json
     '[{"_id":"4ef3accbfa9dcb3a91000004","accent_color":null,"account_id":"4ef3acf1fa9dcb3a9100000a","avatar_id":"4ef3accbfa9dcb3a91000002","background_color":null,"background_image_id":null,"contents":{},"created_at":"2011-12-09T00:50:05-06:00","description":null,"lowercase_slug":"test/hello/world","notify_on_open":false,"password":null,"published":true,"slug":"test/hello/world","theme_id":null,"title":"Hello world","updated_at":"2011-12-23T00:50:05-06:00","published_doc_url":"http://example.com/test/hello/world"}]'
   else
